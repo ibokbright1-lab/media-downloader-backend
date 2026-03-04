8bde84c25aec5df1bcc463e7d62966f2178cc595
ibokbright1-lab <ibokbright1@gmail.com>
2026-03-04 08:16:51 +0100

    Update celery_app.py

--- celery_app.py
+++ celery_app.py
@@ -1,10 +1,11 @@
 import os
 from celery import Celery
 
-redis_url = os.environ.get("REDIS_URL", "redis://localhost:6379")
+redis_url = os.environ.get("REDIS_URL", "redis://red-d6jtkn75r7bs739qu5v0:6379")
 
 celery_app = Celery(
     "tasks",
     broker=redis_url,
     backend=redis_url,
 )
+
